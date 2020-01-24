fb531356c0250b349909ead96e7a56ca912044c9
kazuma1989 <funifuni.1204@gmail.com>
2020-01-24 22:10:18 +0900

    chore: Install csz

--- src/app.tsx
+++ src/app.tsx
@@ -1,9 +1,19 @@
 // Import your web-ready dependencies
 import { h, render } from "/web_modules/preact.js";
+import css from "/web_modules/csz.js";
 
 // Create your main app component
 function SomePreactComponent() {
-  return <h1 style={{ color: "red" }}>Hello, World!</h1>;
+  return (
+    <h1
+      class={css`
+        color: red;
+        font-family: sans-serif;
+      `}
+    >
+      Hello, World!
+    </h1>
+  );
 }
 
 // Inject your application into the an element with the id `app`.
