77c47e5cd01cdc3e4cc6b3f085ac749e8adbc971
kazuma1989 <funifuni.1204@gmail.com>
2020-01-24 22:42:43 +0900

    feat: Apply preact/hooks

--- src/app.tsx
+++ src/app.tsx
@@ -1,15 +1,20 @@
 // Import your web-ready dependencies
 import { h, render } from "/web_modules/preact.js";
+import { useState } from "/web_modules/preact/hooks.js";
 import css from "https://unpkg.com/csz";
 
 // Create your main app component
 function SomePreactComponent() {
+  const [active, setActive] = useState(false);
+  const toggle = () => setActive(v => !v);
+
   return (
     <h1
       class={css`
-        color: red;
+        color: ${active ? "red" : "initial"};
         font-family: sans-serif;
       `}
+      onClick={toggle}
     >
       Hello, World!
     </h1>
